e3326c14c0f1d8cb6b31eda87a0ab03230f75c9b
Pavan Kumar CH <Pavun9848@gmail.com>
2024-05-01 10:49:34 +0530

    few changes

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -34,7 +34,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise e
     
 @ensure_annotations
-def create_directories(path_to_directories: list, verbode = True):
+def create_directories(path_to_directories: list, verbose = True):
     """
     create list of directories
     
